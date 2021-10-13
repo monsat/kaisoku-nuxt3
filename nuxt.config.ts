fae716165ce47f2dd148cc4170b557fa068acd71
mon_sat <tnk@daijiten.info>
2021-10-13 09:33:54 +0900

    Add netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-  server: {
-    port: 3333,
-  },
-  serverMiddleware: [
-    { path: '/api/count', handler: '~/server/api/count' },
-  ],
+  // server: {
+  //   port: 3333,
+  // },
+  // serverMiddleware: [
+  //   { path: '/api/count', handler: '~/server/api/count' },
+  // ],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { useCounter } from '~/composables/useCounter'
 
+const { data } = await useFetch('/api/count')
+
+console.log(data)
+
 const {
   state: counter,
   dbl,
@@ -10,6 +14,8 @@ const {
 
 <template>
   <div>
+    PV: {{ data.pv }}
+    <hr>
     Composable Counter: {{ counter }}（x 2 = {{ dbl }}）<br>
     <button @click="increment">Increment</button>
   </div>
