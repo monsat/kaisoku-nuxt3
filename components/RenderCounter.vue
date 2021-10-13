63e31bbe80bc1b890dcb51bb367224700517ad72
mon_sat <tnk@daijiten.info>
2021-10-13 16:28:56 +0900

    fix: add client only

--- components/RenderCounter.vue
+++ components/RenderCounter.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import { $fetch } from 'ohmyfetch'
 import { format } from 'date-fns'
-import { usePrefetched } from '@/composables/usePrefetched'
 import { RenderCounterReturn } from '@/types'
-
-usePrefetched()
 
 const pv = ref(0)
 const generated = ref('')
@@ -18,13 +15,8 @@ generated.value = format(new Date(renderedOn), 'yyyy-MM-dd HH:mm:SS')
 
 <template>
   <div>
-    <h1>Hello Nuxt 3</h1>
-    <client-only>
-      <RenderCounter/>
-    </client-only>
-    <hr>
-    <TheCounter />
-    <hr>
-    <nuxt-link to="study" :prefetch="true">一覧</nuxt-link>
+    PV: {{ pv }} fetched by Direct API calls using
+    <a href="https://github.com/unjs/ohmyfetch" target="_blank">$fetch</a><br>
+    サーバー側生成時刻 : {{ generated }}
   </div>
 </template>
