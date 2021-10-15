d90dec76720edb4faacfc7d60099b941a30e4bd7
mon_sat <tnk@daijiten.info>
2021-10-15 21:14:30 +0900

    fix: yarn add Windi CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
+  buildModules: [
+    'nuxt-windicss',
+  ],
+  windicss: {
+    analyze: true
+  },
 
   // linkPrefetchedClass: 'nuxt-link-prefetched',
   router: {
