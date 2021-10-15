f4e627f82e9f05d82fed3a18d40551711ca9ac7e
mon_sat <tnk@daijiten.info>
2021-10-15 14:06:22 +0900

    style: remove hash tag

--- components/StudyCardItem.vue
+++ components/StudyCardItem.vue
@@ -12,23 +12,21 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const desc = computed(() => {
-  return props.description.replaceAll(/<.+?>/g, '')
+const desc = computed((maxLen = 64) => {
+  const result = props.description.replaceAll(/<.+?>/g, '').slice(0, maxLen + 1)
+  return result.length > maxLen ? `${result.slice(0, maxLen)}…` : result
 })
 </script>
 
 <template>
   <div class="p-4 lg:w-1/3">
     <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-        {{ props.hash_tag }}
-      </h2>
       <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
         {{ props.title }}
       </h1>
       <p class="leading-relaxed mb-3">
         {{ props.catch }}<br>
-        {{ desc.slice(0, 30) }}
+        {{ desc }}
       </p>
       <a
         :href="props.event_url"
