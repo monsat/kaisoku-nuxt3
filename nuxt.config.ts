31921ba7c32c3511e8f8c897c2e50150cb6b3554
mon_sat <tnk@daijiten.info>
2021-10-15 18:19:56 +0900

    fix: config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
 
-  components: true,
   // linkPrefetchedClass: 'nuxt-link-prefetched',
   router: {
     prefetchLinks: true,
