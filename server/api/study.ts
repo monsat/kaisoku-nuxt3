6d0e4e8cffa255efda9dd61eda1a83d1dacc54f4
mon_sat <tnk@daijiten.info>
2021-10-17 09:39:45 +0900

    fix: revert map method in function

--- server/api/study.ts
+++ server/api/study.ts
@@ -10,8 +10,7 @@ const fetchEvents = async (dt: number) => {
   if (cachedResults?.fetchedOn && dt < cachedResults.fetchedOn + interval) {
     return cachedResults
   }
-  const monthQuery = `ym=202110&` // map 削除
-  // const monthQuery = [addMonths(dt, 0), addMonths(dt, 1), addMonths(dt, 2)].map(d => `ym=${format(d, 'yyyyMM')}`).join('&')
+  const monthQuery = [addMonths(dt, 0), addMonths(dt, 1), addMonths(dt, 2)].map(d => `ym=${format(d, 'yyyyMM')}`).join('&')
   const events: StudyResult['events'] = await fetch(`${url}${monthQuery}`)
     .then(res => res.json())
     .then(results => (results.events as StudyResult['events'][]).reverse())
