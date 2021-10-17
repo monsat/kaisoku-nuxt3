2d4b0b91928c85b34bc1c208d5f090639e2770ee
mon_sat <tnk@daijiten.info>
2021-10-17 19:51:11 +0900

    feat: add last update

--- components/The/Footer.vue
+++ components/The/Footer.vue
@@ -5,10 +5,13 @@
         <BaseNuxtLink/>
       </slot>
     </div>
-    <div class="text-center">
+    <div class="mt-4 text-center">
       <BaseLink href="https://twitter.com/ktanaka">@ktanaka</BaseLink>
       ／
       <BaseLink href="https://www.coworking.tokyo.jp/">コワーキングスペース茅場町 Co-Edo</BaseLink>
+    </div>
+    <div class="text-right">
+      Webサイト最終更新日 : 2021-10-17
     </div>
   </footer>
 </template>
