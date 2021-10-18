0c69aba82307f396c614be69779be0e4839f04a9
mon_sat <tnk@daijiten.info>
2021-10-18 12:40:26 +0900

    chore: add color palettes

--- windi.config.js
+++ windi.config.js
@@ -16,6 +16,25 @@ export default {
         default: '#00DC82',
         lighter: '#80eec0',
         100: '#ccf8e6',
+        // https://colorsui.com/color-shades-generator/
+        // '#00160d',
+        // '#002c1a',
+        // '#004227',
+        // '#005834',  // dark
+        // '#006e41',
+        // '#00844e',
+        // '#009a5b',  // header
+        // '#00b068',
+        // '#00c675',
+        // '#00dc82',  // default
+        // '#1ae08f',
+        // '#33e39b',
+        // '#4de7a8',
+        // '#66eab4',
+        // '#80eec1',  // lighter
+        // '#99f1cd',
+        // '#b3f5da',
+        // '#ccf8e6',  // 100
       },
     },
     extend: {
