23de37629f97a7f09842e240713e8e85a1e78cfe
mon_sat <tnk@daijiten.info>
2021-10-18 12:09:49 +0900

    fix: hydration

--- components/Base/Info.vue
+++ components/Base/Info.vue
@@ -4,7 +4,7 @@
       <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
       <div>
         <p class="leading-8 font-bold"><slot name="head" /></p>
-        <p class="pt-1"><slot name="default" /></p>
+        <div class="pt-1"><slot name="default" /></div>
       </div>
     </div>
   </div>
