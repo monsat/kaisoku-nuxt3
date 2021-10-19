5a5e4539eaf1d7a28d65e82bc430faf9f53282d5
mon_sat <tnk@daijiten.info>
2021-10-19 20:54:22 +0900

    style: bold for catch copy

--- components/StudyCardItem.vue
+++ components/StudyCardItem.vue
@@ -43,8 +43,10 @@ const moveTo = () => {
           </h1>
         </div>
         <div>
-          <p class="leading-relaxed">
+          <p class="font-bold">
             {{ props.catch }}<br>
+          </p>
+          <p class="leading-relaxed">
             {{ $stripTags(props.description) }}
           </p>
           <p class="text-right">
