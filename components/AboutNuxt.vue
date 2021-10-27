ca9edbb0ea03bf3bd6077cd9de9e3c4990a7f48c
mon_sat <tnk@daijiten.info>
2021-10-27 13:27:36 +0900

    fix: nuxt official brand name

--- components/AboutNuxt.vue
+++ components/AboutNuxt.vue
@@ -4,13 +4,13 @@
       Nuxt 3 について
     </TheH2>
     <p>
-      <BaseLink href="https://nuxtjs.org/">Nuxt.js</BaseLink> は <BaseLink href="https://v3.ja.vuejs.org/">Vue.js</BaseLink> をベースに開発された、フロントエンド開発者・ Web アプリケーションの開発者が高速で高機能な Web サイトの構築を可能にするフレームワークです。
+      <BaseLink href="https://nuxtjs.org/">Nuxt</BaseLink> は <BaseLink href="https://v3.ja.vuejs.org/">Vue.js</BaseLink> をベースに開発された、フロントエンド開発者・ Web アプリケーションの開発者が高速で高機能な Web サイトの構築を可能にするフレームワークです。
     </p>
     <p>
       CoC（設定より規約）の考え方にもとづき、最小限の設定のみで、多数のベストプラクティスな実装による機能により、豊かな開発体験が得られます。
     </p>
     <p class="mb-8">
-      Vue.js 単体での開発では少なからず時間の要する、 SSR や ルーティングといったアプリケーション開発に欠かせない機能を、網羅的に提供しているのが Nuxt.js です。
+      Vue.js 単体での開発では少なからず時間の要する、 SSR や ルーティングといったアプリケーション開発に欠かせない機能を、網羅的に提供しているのが Nuxt です。
     </p>
     <TheH2>
       Nuxt 3 の特徴
--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ const { url: ogpImageUrl } = useOgpImage(title)
     <TheH1> Nuxt 3 パブリック Beta リリース 🎉</TheH1>
     <p>
       2021年10月12日（火）の23:00（日本時間）に、待望の
-      <BaseLink href="https://nuxtjs.org/announcements/nuxt3-beta/">Nuxt 3 public beta（Nuxt.js 3.0.0-beta）がリリース</BaseLink>
+      <BaseLink href="https://nuxtjs.org/announcements/nuxt3-beta/">Nuxt 3 public beta（Nuxt 3.0.0-beta）がリリース</BaseLink>
       されました。
     </p>
     <p>
