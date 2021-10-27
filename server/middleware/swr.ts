d917c0adebc48bc33d6d0ca970113f2925446af1
mon_sat <tnk@daijiten.info>
2021-10-27 14:12:08 +0900

    feat: add middleware

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -1,6 +1,10 @@
 import type { IncomingMessage, ServerResponse } from 'http'
+import { parseURL } from 'ufo'
 
 export default async (req: IncomingMessage, res: ServerResponse, next: (err?: Error) => any) => {
-  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate')
+  const { pathname } = parseURL(req.url)
+  if (pathname.startsWith('/api/study')) {
+    res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate')
+  }
   next()
 }
