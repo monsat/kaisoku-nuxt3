0386ee8b47235a63e3275b202d21ef61debf52e2
mon_sat <tnk@daijiten.info>
2021-10-27 13:28:28 +0900

    refactor: use useRuntimeConfig

--- composables/useOgpImage.ts
+++ composables/useOgpImage.ts
@@ -3,7 +3,7 @@ import { PublicRuntimeConfig } from '@/types'
 const url = ref('')
 
 const setOgpImageUrl = (title: string, maxLen: number) => {
-  const { $config } = useNuxtApp() as unknown as { $config: PublicRuntimeConfig }
+  const $config = useRuntimeConfig() as PublicRuntimeConfig
   const origin = process.dev ? $config.develop.url : $config.production.url
   const path = '/api/ogp/'
   const filename = encodeURIComponent(title.slice(0, maxLen))
