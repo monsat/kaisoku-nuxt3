6b5c6a57761f931e43b54b16435cf43df36a7195
rie-oki-agn <rieoki.style.agn@gmail.com>
2022-05-26 12:15:05 +0900

    content dir & about added

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    About me
-  </div>
+  <main>
+    <ContentDoc v-slot="{ doc }">
+      <TheH1>
+          {{ doc.title }}
+      </TheH1>
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+  </main>
 </template>
